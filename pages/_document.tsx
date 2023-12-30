88b28d36562d18c5994862b8df6f651a15457af7
bejewel-seungtae <seungtae@bejewel.kr>
2023-12-30 17:30:12 +0900

    폰트 적용

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -41,6 +41,39 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function (d) {
+          var config = {
+              kitId: "pny6mch",
+              scriptTimeout: 3000,
+              async: true,
+            },
+            h = d.documentElement,
+            t = setTimeout(function () {
+              h.className =
+                h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+            }, config.scriptTimeout),
+            tk = d.createElement("script"),
+            f = false,
+            s = d.getElementsByTagName("script")[0],
+            a;
+          h.className += " wf-loading";
+          tk.src = "https://use.typekit.net/" + config.kitId + ".js";
+          tk.async = true;
+          tk.onload = tk.onreadystatechange = function () {
+            a = this.readyState;
+            if (f || (a && a != "complete" && a != "loaded")) return;
+            f = true;
+            clearTimeout(t);
+            try {
+              Typekit.load(config);
+            } catch (e) {}
+          };
+          s.parentNode.insertBefore(tk, s);
+        })(document)`,
+          }}
+        ></script>
         <body>
           <Main />
           <NextScript />
