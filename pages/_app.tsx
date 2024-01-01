da02b146c8ac94f0a6ba7fb86e791db8b95bb149
bejewel-seungtae <seungtae@bejewel.kr>
2024-01-01 20:18:24 +0900

    카카오 대응

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import GlobalStyle from "../globalStyle";
 import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  useEffect(() => {
+  //@ts-ignore
+  if (typeof Kakao !== "undefined") {
     //@ts-ignore
-    if (Kakao && !Kakao.isInitialized())
+    if (!Kakao.isInitialized()) {
       //@ts-ignore
       Kakao.init("6e3c263d4c0db34a0644c6b5fab6dac3");
-  }, []);
+    }
+  }
 
   return (
     <>
